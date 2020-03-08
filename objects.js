5793795a0a19a19ac8a8ecb79314df87d7397796
D D <ddeljanin5@icloud.com>
2020-03-08 19:01:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,7 +6,12 @@ playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return 
+  // Add the song and artist as a key-value pair and then return the whole playlist
+  playlist.assign(
+    {},
+    artistName,
+    songTitle
+    ); 
 }
 
 function removeFromPlaylist(playlist, artistName) {
